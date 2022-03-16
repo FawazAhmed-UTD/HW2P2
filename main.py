82a33f15bff46bf454c68161815e1cda5fa2f5e2
Fawaz Ahmed <52136572+FawazAhmed-UTD@users.noreply.github.com>
2022-03-16 17:29:10 -0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+Author: Fawaz Ahmed & Kevin
+NetId: fxa180017
+Date: 3/16/2022
+Version: 1.0
+Notes: Main program for HW2P2
+"""
+import sys
+
+
+def main():
+    print('Number of arguments:', len(sys.argv), 'arguments.')
+    print('Argument List:', str(sys.argv))
+
+
+if __name__ == '__main__':
+    main()
